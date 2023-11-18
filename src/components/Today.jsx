20b5d7b1ce4bb9d9b6d75f35b0e6f0f840c1d3d1
Jonash Gania <jonashtiglao@gmail.com>
2023-11-18 23:42:20 +0800

    added input form

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { FaTrashAlt } from "react-icons/fa";
 
 export default function Today({todo}) {
   return (
-    <li>
-        <div>
-            
+    <li className='flex items-center w-full justify-between'>
+        <div className='flex items-center gap-4'>
+          <input 
+            type="checkbox" 
+            className='cursor-pointer'
+          />
+          <span>{todo}</span>
+        </div>
+        <div className='cursor-pointer'>
+          <FaTrashAlt />
         </div>
     </li>
   )
