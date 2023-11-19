708bc1a2d2d04cf6e325e08422a8b1d4b64e9e89
Jonash Gania <jonashtiglao@gmail.com>
2023-11-19 15:21:26 +0800

    Render task lists

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -3,16 +3,23 @@ import { FaTrashAlt } from "react-icons/fa";
 
 export default function Today({todo}) {
   return (
-    <li className='flex items-center w-full justify-between'>
+    <li className='flex items-center w-full justify-between hover:bg-zinc-200 transition-all duration-200 ease-in cursor-pointer px-4 py-1 rounded-md'>
         <div className='flex items-center gap-4'>
           <input 
             type="checkbox" 
             className='cursor-pointer'
           />
-          <span>{todo}</span>
+          <div className='flex flex-col'>
+            <span className='font-medium text-lg'>{todo.tasks}</span>
+            <span className='text-sm text-green-500'>{todo.date}</span>
+          </div>
+          
         </div>
-        <div className='cursor-pointer'>
-          <FaTrashAlt />
+        <div className='flex items-center gap-8'>
+          <span>{todo.label}</span>
+          <div className='cursor-pointer'>
+            <FaTrashAlt />
+        </div>
         </div>
     </li>
   )
