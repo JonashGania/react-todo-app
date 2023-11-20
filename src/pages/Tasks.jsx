50c84a993500f1c3e204495b465641d6d1f5dbac
Jonash Gania <jonashtiglao@gmail.com>
2023-11-20 16:52:01 +0800

    added Tasks page route

---
+++ src/pages/Tasks.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TaskForm from '../components/TasksForm'
+
+export default function Tasks() {
+  return (
+    <div className=''>
+        <TaskForm />
+    </div>
+  )
+}
