e5fadbf591b0c8a4a2c64d5c705d8f4b337a56e3
Jonash Gania <jonashtiglao@gmail.com>
2023-11-20 16:48:42 +0800

    added Router to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
-import TdTaskForm from './components/TdTaskForm'
-
+import { Routes, Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Tasks from './pages/Tasks'
 function App() {
 
   return (
-    <div className='w-full h-screen'>
-      <TdTaskForm />
+    <div className='w-full h-screen bg-[rgb(3,21,37)]'>
+      <Routes>
+        <Route path='/' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
+        <Route path='/tasks' element={<Tasks />}/>
+      </Routes>
     </div>
   )
 }
