0afa6154ef836639806de168bbb4da107bd0e749
Jonash Gania <jonashtiglao@gmail.com>
2023-11-22 23:32:30 +0800

    added onchange toggleComplete

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { FaTrashAlt } from "react-icons/fa";
 
-export default function Today({todo}) {
+export default function Today({todo, toggleComplete}) {
   return (
-    <li className='flex items-center w-full justify-between hover:bg-zinc-200 transition-all duration-200 ease-in cursor-pointer px-4 py-1 rounded-md'>
+    <li
+      onClick={() => toggleComplete(todo)} 
+      className='flex items-center w-full justify-between hover:bg-zinc-200 transition-all duration-200 ease-in cursor-pointer px-4 py-1 rounded-md'
+    >
         <div className='flex items-center gap-4'>
           <input 
             type="checkbox" 
             className='cursor-pointer'
+            checked={todo.completed ? 'checked' : ''}
+            onChange={() => toggleComplete(todo)}
           />
           <div className='flex flex-col'>
-            <span className='font-medium text-lg'>{todo.task}</span>
-            <span className='text-sm text-green-500'>{todo.date}</span>
+            <span className={`font-medium text-lg ${todo.completed ? 'text-gray-400 line-through' : 'text-black'}`}>{todo.task}</span>
+            <span className={`text-sm ${todo.completed ? 'text-green-400 line-through' : 'text-green-500'}`}>{todo.date}</span>
           </div>
           
         </div>
