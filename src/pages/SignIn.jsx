a92ddec8c4f11a44741ede5f3cafd4a1a1b0d542
Jonash Gania <jonashtiglao@gmail.com>
2023-11-22 23:33:24 +0800

    changed button hover bg color

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -33,6 +33,7 @@ export default function SignIn() {
                             type="text" 
                             placeholder='Email'
                             className='outline-none text-gray-600 w-full'
+                            required
                         />
                     </div>
                     <div className='flex items-center w-full gap-3 px-2 py-2 border border-gray-400 rounded-lg'>
@@ -44,13 +45,14 @@ export default function SignIn() {
                             type="password" 
                             placeholder='Password'
                             className='outline-none text-gray-600 w-full'
+                            required
                         />
                     </div>
                     <button className='mt-5 text-lg px-1 py-2 bg-sky-600 hover:bg-sky-700 rounded-xl text-white active:scale-110  transition-all duration-200 ease-in'>Sign In</button>
                 </div>
                 <span className='text-gray-500 py-4'>Or</span>
                 <button 
-                    className='cursor-pointer hover:bg-gray-300 max-w-xs w-full px-2 py-2 rounded-lg flex justify-center transition-all duration-200 ease-in'
+                    className='cursor-pointer hover:bg-[rgba(0,0,0,0.2)] max-w-xs w-full px-2 py-2 rounded-lg flex justify-center transition-all duration-200 ease-in'
                     onClick={signWithGoogle}
                 >   
                     <FcGoogle 
