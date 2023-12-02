132bf49ecfda2226b4486d80c34698894b0f6f39
Jonash <jonashtiglao@gmail.com>
2023-12-02 16:12:21 +0800

    Added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { Routes, Route } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Tasks from './pages/Tasks'
+import Pomodoro from './pages/Pomodoro'
+import Navbar from './components/Navbar'
 function App() {
 
   return (
     <div className='w-full h-screen bg-[rgb(3,21,37)]'>
+      <Navbar />
+
       <Routes>
         <Route path='/' element={<SignIn />}/>
         <Route path='/sign-up' element={<SignUp />}/>
         <Route path='/tasks' element={<Tasks />}/>
+        <Route path='/pomodoro' element={<Pomodoro/>}/>
       </Routes>
     </div>
   )
