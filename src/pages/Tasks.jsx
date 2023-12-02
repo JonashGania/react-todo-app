02da91142f0a7e71c2beb34f3d66d61e32b0b825
Jonash <jonashtiglao@gmail.com>
2023-12-02 16:12:55 +0800

    remove extra div

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -42,32 +42,30 @@ export default function Tasks() {
     }
 
     return (
-        <div className=''>
-        <div className='bg-[rgb(3,21,37)] pt-32'>
-                <div className='max-w-2xl mx-auto bg-white px-4 py-4 rounded-lg'>
-                    <div className='px-4 flex justify-center flex-col'>
-                        <h1 className='text-3xl font-bold pb-8 text-center'>My Tasks</h1>
-                        <ul className='w-[500px] flex gap-4 flex-col pb-8 mx-auto'>
-                            {todos.map((task, index) => (
-                                <Today 
-                                    key={index} 
-                                    todo={task} 
-                                    toggleComplete={toggleComplete}
-                                    deleteTodo={deleteTodo}
-                                />
-                            ))}
-                        </ul>
-                        {isOpen && <Modal onClose={handleModalClose}/>}
-                        <button 
-                            className='flex items-center gap-2 text-lg px-3 py-3 bg-sky-600 hover:bg-sky-700 rounded-md text-white w-[125px] active:scale-110  transition-all duration-200 ease-in '
-                            onClick={handleModalOpen}
-                        >
-                            <FaPlus size={'1.1rem'} color='white'/>
-                            Add Task
-                        </button>
-                    </div>
+        <div className='bg-[rgb(3,21,37)]'>
+            <div className='max-w-2xl mx-auto bg-white px-4 py-4 rounded-lg'>
+                <div className='px-4 flex justify-center flex-col'>
+                    <h1 className='text-3xl font-bold pb-8 text-center'>My Tasks</h1>
+                    <ul className='w-[500px] flex gap-4 flex-col pb-8 mx-auto'>
+                        {todos.map((task, index) => (
+                            <Today 
+                                key={index} 
+                                todo={task} 
+                                toggleComplete={toggleComplete}
+                                deleteTodo={deleteTodo}
+                            />
+                        ))}
+                    </ul>
+                    {isOpen && <Modal onClose={handleModalClose}/>}
+                    <button 
+                        className='flex items-center gap-2 text-lg px-3 py-3 bg-sky-600 hover:bg-sky-700 rounded-md text-white w-[125px] active:scale-110  transition-all duration-200 ease-in '
+                        onClick={handleModalOpen}
+                    >
+                        <FaPlus size={'1.1rem'} color='white'/>
+                        Add Task
+                    </button>
                 </div>
-            </div>   
-        </div>
+            </div>
+        </div>   
     )
 }
