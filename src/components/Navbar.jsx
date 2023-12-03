a19e24eb12088f1e91ba1fe018258c074c5c21d7
Jonash <jonashtiglao@gmail.com>
2023-12-03 16:48:53 +0800

    changed navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaTasks } from "react-icons/fa";
-import { IoIosTimer } from "react-icons/io";
+import { Link, NavLink } from 'react-router-dom'
+
 
 export default function Navbar() {
   return (
     <header className='max-w-2xl mx-auto'>
         <nav className='w-full h-32 flex items-center justify-center gap-20'>
-            <Link
+            <NavLink
                 to='/tasks'
-                className='border-[3px] border-sky-500 rounded-[50%] w-16 h-16 hover:px-[2px] hover:py-[2px] duration-100 transition-all ease-in flex items-center justify-center'
+                className='text-white text-2xl font-semibold px-3 relative'
             >
-                <div className='w-full h-full flex items-center justify-center rounded-[50%] bg-gray-200'>
-                    <FaTasks 
-                        size='2.3rem'
-                        color='rgb(2,132,199)'
-                    />
-                </div>
-            </Link>
-            <Link
+                My Tasks
+            </NavLink>
+            <NavLink
                 to='/pomodoro'
-                className='border-[3px] border-sky-500 rounded-[50%] w-16 h-16 hover:px-[2px] hover:py-[2px] duration-100 transition-all ease-in flex items-center justify-center'
+                className='text-white text-2xl font-semibold px-3 relative'
             >
-                <div className='w-full h-full flex items-center justify-center rounded-[50%] bg-gray-200'>
-                    <IoIosTimer 
-                        size='2.5rem'
-                        color='rgb(2,132,199)'
-                    />
-                </div>
-            </Link>
+                Pomodoro
+            </NavLink>
         </nav>
     </header>
   )
