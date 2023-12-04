699175bce21dc9382089680f205e70506a7ec04e
Jonash <jonashtiglao@gmail.com>
2023-12-04 15:05:32 +0800

    added user sign up function

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,39 +1,97 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
+
 
 export default function SignUp() {
-  return (
-    <div className='flex justify-center items-center w-full h-screen'>
-        <div className='bg-white rounded-lg px-4 pt-12 pb-4 max-w-md w-full flex justify-center flex-col items-center'>
-            <h3 className='text-2xl font-semibold pb-6'>Sign Up</h3>
-            <div className='max-w-xs w-full flex flex-col gap-4'>
-                <input 
-                    type="text" 
-                    placeholder='Full name'
-                    className='outline-none text-gray-600 w-full px-2 py-2 border border-gray-400 rounded-lg'
-                />
-                <input 
-                    type="text" 
-                    placeholder='Email'
-                    className='outline-none text-gray-600 w-full px-2 py-2 border border-gray-400 rounded-lg'
-                />
-                <input 
-                    type="password" 
-                    placeholder='Password'
-                    className='outline-none text-gray-600 w-full px-2 py-2 border border-gray-400 rounded-lg'
-                />
-                <button className='mt-5 text-lg px-1 py-2 bg-sky-600 hover:bg-sky-700 rounded-xl text-white active:scale-110  transition-all duration-200 ease-in'>Sign Up</button>
-            </div>
-            <p className='text-sm pt-12'>
-                Already have an account?
-                <Link
-                    to="/"
-                    className='font-bold pl-2'
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSignUp = (e) => {
+        const id = e.target.id;
+        const value = e.target.value;
+
+        switch(id){
+            case 'fullName':
+                setFullName(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const signUpWithEmailPass = async(e) => {
+        e.preventDefault(e);
+
+        try{
+            const user = await createUserWithEmailAndPassword(auth, email, password);
+            setFullName('');
+            setEmail('');
+            setPassword('');
+            console.log(user);
+            navigate('/');
+            
+        } catch(error){
+            console.error('Error Signing-up', error)
+        }
+    }
+
+    return (
+        <div className='flex justify-center items-center w-full h-screen'>
+            <div className='bg-white rounded-lg px-4 pt-12 pb-4 max-w-md w-full flex justify-center flex-col items-center'>
+                <h3 className='text-2xl font-semibold pb-6'>Sign Up</h3>
+                <form 
+                    className='max-w-xs w-full' 
+                    onSubmit={signUpWithEmailPass}
                 >
-                    Sign in
-                </Link>  
-            </p>
+                    <div className='w-full flex flex-col gap-4'>
+                        <input 
+                            type="text" 
+                            placeholder='Full name'
+                            className='outline-none text-gray-600 w-full px-2 py-2 border border-gray-400 rounded-lg'
+                            id='fullName'
+                            onChange={handleSignUp}
+                            required
+                        />
+                        <input 
+                            type="text" 
+                            placeholder='Email'
+                            className='outline-none text-gray-600 w-full px-2 py-2 border border-gray-400 rounded-lg'
+                            id='email'
+                            onChange={handleSignUp}
+                            required
+                        />
+                        <input 
+                            type="password" 
+                            placeholder='Password'
+                            className='outline-none text-gray-600 w-full px-2 py-2 border border-gray-400 rounded-lg'
+                            id='password'
+                            onChange={handleSignUp}
+                            required
+                       />
+                        <button className='mt-5 text-lg px-1 py-2 bg-sky-600 hover:bg-sky-700 rounded-xl text-white  transition-all duration-200 ease-in'>Sign Up</button>
+                    </div>
+                </form>
+                <p className='text-sm pt-12'>
+                    Already have an account?
+                    <Link
+                        to="/"
+                        className='font-bold pl-2'
+                    >
+                        Sign in
+                    </Link>  
+                </p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
