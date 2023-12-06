b95dcc363656738f9680e97483b9ec91c53b1351
Jonash <jonashtiglao@gmail.com>
2023-12-06 15:05:14 +0800

    changed firebase import path

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
-import { auth } from '../firebase';
+import { auth } from '../utils/firebase';
 
 
 export default function SignUp() {
