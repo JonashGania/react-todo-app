0295fc666cb70167da97a5ab2decaabaf1b88fba
Jonash <jonashtiglao@gmail.com>
2023-12-06 15:03:50 +0800

    changed firebase import path

--- src/components/SignOut.jsx
+++ src/components/SignOut.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { signOut } from 'firebase/auth'
 import { MdOutlineLogout } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
-import { auth } from '../firebase';
+import { auth } from '../utils/firebase';
 
 
 export default function SignOut() {
