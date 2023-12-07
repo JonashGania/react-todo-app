e30089026bbf5d06661f801cb2d01b42ecb38f7f
Jonash <jonashtiglao@gmail.com>
2023-12-07 16:27:32 +0800

    validate email and pass

---
+++ src/utils/emailPassValidation.js
@@ -0,0 +1,9 @@
+export const validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return regex.test(password);
+}
+
+export const validateEmail = (email) => {
+    const regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    return regex.test(email);
+}
