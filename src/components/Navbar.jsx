ddb5b21db4b9089ded0d1bdb44484d2fe7903786
Jonash <jonashtiglao@gmail.com>
2023-12-08 19:57:01 +0800

    small change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import useGetUserInfo from '../hooks/UseGetUserInfo'
-import SignOut from './SignOut';
 import Dropdown from './Dropdown';
 import { IoChevronDownOutline } from "react-icons/io5";
 
 
 export default function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const { name, profilePhoto } = useGetUserInfo();
+    const userInfo = useGetUserInfo();
 
     const handleDropdownOpen = (e) => {
         e.stopPropagation();
@@ -26,9 +25,9 @@ export default function Navbar() {
                     onClick={handleDropdownOpen}
                     className={`flex items-center gap-2 cursor-default justify-end py-1 px-1 rounded-3xl transition-all duration-100 ease-in group ${isDropdownOpen ? 'bg-sky-700 hover:bg-sky-700' : 'bg-[rgb(25,38,49)] hover:bg-black'}`}
                 >
-                    {profilePhoto ? (
+                    {userInfo?.photoURL ? (
                         <img 
-                            src={profilePhoto} 
+                            src={userInfo.photoURL} 
                             alt="Avatar" 
                             className='rounded-[50%] w-7'    
                         />
@@ -45,7 +44,7 @@ export default function Navbar() {
                         className={`group-hover:translate-y-1 duration-100 transition ease-in ${isDropdownOpen ? 'translate-y-1' : ''}`}
                     />
                 </button>
-                {isDropdownOpen && <Dropdown name={name} profilePhoto={profilePhoto} onClose={handleDropdownClose}/>}
+                {isDropdownOpen && <Dropdown name={userInfo.name} profilePhoto={userInfo.photoURL} onClose={handleDropdownClose}/>}
             </div>
             <nav className='w-full h-16 pb-20 pt-10 flex items-center justify-center gap-20'>
                 <NavLink
