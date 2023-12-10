3f6f321df58d3837ffd977caf2c51e311790f65f
Jonash <jonashtiglao@gmail.com>
2023-12-10 22:47:32 +0800

    changed bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,46 +19,39 @@ export default function Navbar() {
     }
 
     return (
-        <header className='max-w-4xl mx-auto'>
-            <div className='py-4 flex justify-end items-center relative'>
-                <button 
-                    onClick={handleDropdownOpen}
-                    className={`flex items-center gap-2 cursor-default justify-end py-1 px-1 rounded-3xl transition-all duration-100 ease-in group ${isDropdownOpen ? 'bg-sky-700 hover:bg-sky-700' : 'bg-[rgb(25,38,49)] hover:bg-black'}`}
-                >
-                    {userInfo?.photoURL ? (
-                        <img 
-                            src={userInfo.photoURL} 
-                            alt="Avatar" 
-                            className='rounded-[50%] w-7'    
+        <header className='w-full bg-green-700'>
+            <nav className='max-w-4xl mx-auto flex items-center justify-between py-2'>
+                <div className='flex items-center gap-2'>
+                    <img src="/logo.png" alt="logo" className=' w-12'/>
+                    <p className='text-lg text-white font-medium'>PomoTask</p>
+                </div>
+                <div className='py-2 flex justify-end items-center relative'>
+                    <button 
+                        onClick={handleDropdownOpen}
+                        className={`flex items-center gap-2 cursor-default justify-end py-1 px-1 rounded-3xl transition-all duration-100 ease-in group ${isDropdownOpen ? 'bg-black hover:bg-black' : 'bg-[#ffffff8c] hover:bg-black'}`}
+                    >
+                        {userInfo?.photoURL ? (
+                            <img 
+                                src={userInfo.photoURL} 
+                                alt="Avatar" 
+                                className='rounded-[50%] w-7'    
+                            />
+                        ) : (
+                            <img 
+                                src="/profile.png" 
+                                alt="Avatar" 
+                                className='rounded-[50%] w-7'
+                            />
+                        )}
+                        <IoChevronDownOutline
+                            color='white'
+                            size='1rem'
+                            className={`group-hover:translate-y-1 duration-100 transition ease-in ${isDropdownOpen ? 'translate-y-1' : ''}`}
                         />
-                    ) : (
-                        <img 
-                            src="/profile.png" 
-                            alt="Avatar" 
-                            className='rounded-[50%] w-7'
-                        />
-                    )}
-                    <IoChevronDownOutline
-                        color='white'
-                        size='1rem'
-                        className={`group-hover:translate-y-1 duration-100 transition ease-in ${isDropdownOpen ? 'translate-y-1' : ''}`}
-                    />
-                </button>
-                {isDropdownOpen && <Dropdown name={userInfo.name} profilePhoto={userInfo.photoURL} onClose={handleDropdownClose}/>}
-            </div>
-            <nav className='w-full h-16 pb-20 pt-10 flex items-center justify-center gap-20'>
-                <NavLink
-                    to='/tasks'
-                    className='text-white text-2xl font-semibold px-3 relative'
-                >
-                    My Tasks
-                </NavLink>
-                <NavLink
-                    to='/pomodoro'
-                    className='text-white text-2xl font-semibold px-3 relative'
-                >
-                    Pomodoro
-                </NavLink>
+                    </button>
+                    {isDropdownOpen && <Dropdown name={userInfo.name} profilePhoto={userInfo.photoURL} onClose={handleDropdownClose}/>}
+                </div>
+                
             </nav>
         </header>
     )
