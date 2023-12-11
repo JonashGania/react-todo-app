3b44e41ec8f88f49f1502c8f61ddabd3e344ac78
Jonash <jonashtiglao@gmail.com>
2023-12-11 20:13:35 +0800

    import Checkbox

--- src/components/RenderTasks.jsx
+++ src/components/RenderTasks.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { FaTrashAlt } from "react-icons/fa";
+import Checkbox from './Checkbox';
 
 export default function RenderTasks({ todo, toggleComplete, deleteTodo }) {
 
@@ -10,12 +11,13 @@ export default function RenderTasks({ todo, toggleComplete, deleteTodo }) {
       className={`flex items-center w-full justify-between hover:bg-zinc-200  px-4 py-1 rounded-md ${todo.completed ? 'bg-zinc-200' :'bg-zinc-transparent'}`}
     >
         <div className='flex items-center gap-4'>
-          <input 
+          {/* <input 
             type="checkbox" 
             className='cursor-pointer'
             checked={todo.completed ? 'checked' : ''}
             onChange={() => toggleComplete(todo)}
-          />
+          /> */}
+          <Checkbox completed={todo.completed} toggleComplete={() => toggleComplete(todo)}/>
           <div className='flex flex-col'>
             <span className={`font-medium text-lg ${todo.completed ? 'text-gray-400 line-through' : 'text-black'}`}>{todo.task}</span>
             <span className={`text-sm ${todo.completed ? 'text-green-400 line-through' : 'text-green-500'}`}>{todo.date}</span>
