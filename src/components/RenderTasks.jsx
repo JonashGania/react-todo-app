9f21ef1436d499fe5868b7ebc9ec89bac0195443
Jonash <jonashtiglao@gmail.com>
2023-12-28 15:39:11 +0800

    fix mobile view

--- src/components/RenderTasks.jsx
+++ src/components/RenderTasks.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 import { FaTrashAlt } from "react-icons/fa";
 import Checkbox from './Checkbox';
@@ -8,18 +7,18 @@ export default function RenderTasks({ todo, toggleComplete, deleteTodo }) {
   return (
     <li
       onClick={() => toggleComplete(todo)} 
-      className={`flex items-center w-full justify-between hover:bg-zinc-200  px-4 py-1 rounded-md ${todo.completed ? 'bg-zinc-200' :'bg-zinc-transparent'}`}
+      className={`flex items-center w-full justify-between hover:bg-zinc-200 px-2 sm:px-4 py-1 rounded-md ${todo.completed ? 'bg-zinc-200' :'bg-zinc-transparent'}`}
     >
-        <div className='flex items-center gap-4'>
+        <div className='flex items-center gap-2 sm:gap-4'>
           <Checkbox completed={todo.completed} toggleComplete={() => toggleComplete(todo)}/>
           <div className='flex flex-col'>
-            <span className={`font-medium text-lg ${todo.completed ? 'text-gray-400 line-through' : 'text-black'}`}>{todo.task}</span>
-            <span className={`text-sm ${todo.completed ? 'text-green-400 line-through' : 'text-green-500'}`}>{todo.date}</span>
+            <span className={`font-medium sm:text-lg text-base ${todo.completed ? 'text-gray-400 line-through' : 'text-black'}`}>{todo.task}</span>
+            <span className={`text-xs sm:text-sm ${todo.completed ? 'text-green-400 line-through' : 'text-green-500'}`}>{todo.date}</span>
           </div>
           
         </div>
-        <div className='flex items-center gap-8'>
-          <span>{todo.label}</span>
+        <div className='flex items-center gap-4 sm:gap-8'>
+          <span className=' text-sm sm:text-base'>{todo.label}</span>
           <div 
             className='cursor-pointer'
             onClick={() => deleteTodo(todo.id)}
