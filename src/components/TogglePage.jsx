1bfdcbe8c2131222e02f16f9e256cfc94846f243
Jonash <jonashtiglao@gmail.com>
2023-12-28 15:39:20 +0800

    fix mobile view

--- src/components/TogglePage.jsx
+++ src/components/TogglePage.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default function TogglePage() {
   return (
-    <div className='w-full h-16 pb-20 pt-10 flex items-center justify-center gap-20'>
+    <div className='w-full h-16 pb-20 pt-10 flex items-center justify-center gap-14 sm:gap-20 px-4 transition-all duration-300 ease-in'>
         <NavLink
             to='/tasks'
-            className='text-black text-2xl font-semibold px-3 relative'
+            className='text-black text-lg sm:text-2xl font-semibold px-3 relative transition-all duration-300 ease-in'
         >
             My Tasks
         </NavLink>
         <NavLink
             to='/pomodoro'
-            className='text-black text-2xl font-semibold px-3 relative'
+            className='text-black text-lg sm:text-2xl font-semibold px-3 relative transition-all duration-300 ease-in'
         >
             Pomodoro
         </NavLink>
