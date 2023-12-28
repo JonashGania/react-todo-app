7bfb482feb93e13e834604c600abbc39283f90aa
Jonash <jonashtiglao@gmail.com>
2023-12-28 15:40:05 +0800

    fix mobile view

--- src/pages/Pomodoro.jsx
+++ src/pages/Pomodoro.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import TogglePage from '../components/TogglePage';
 import { MdRestartAlt } from "react-icons/md";
@@ -59,44 +59,44 @@ export default function Pomodoro() {
     <div>
       <Navbar />
       <TogglePage />
-      <div className='max-w-2xl mx-auto pt-5'>
-        <div className='flex items-center justify-center gap-6 z-20 bg-[rgb(25,38,49)] max-w-[445px] mx-auto py-2 rounded-3xl'>
+      <div className='max-w-2xl mx-auto pt-5 px-4'>
+        <div className='flex items-center justify-center gap-3 sm:gap-6 z-20 bg-[rgb(25,38,49)] transition-all duration-200 ease-in sm:max-w-[445px] max-w-[295px] mx-auto py-[6px] sm:py-2 rounded-3xl'>
           <button
             onClick={() => switchMode('work', modeDurations.work)} 
-            className={`px-6 py-2 text-xl font-semibold rounded-3xl ${mode === 'work' ? 'bg-white text-black' : 'bg-transparent text-white'}`}
+            className={` px-4 sm:px-6 py-2 text-sm sm:text-xl font-semibold rounded-3xl transition-all duration-200 ease-in ${mode === 'work' ? 'bg-white text-black' : 'bg-transparent text-white'}`}
           >
             Work
           </button>
           <button 
             onClick={() => switchMode('shortBreak', modeDurations.shortBreak)} 
-            className={`px-5 py-2 text-xl font-semibold rounded-3xl ${mode === 'shortBreak' ? 'bg-white text-black' : 'bg-transparent text-white'}`}
+            className={`px-3 sm:px-5 py-2 text-sm sm:text-xl font-semibold rounded-3xl transition-all duration-200 ease-in ${mode === 'shortBreak' ? 'bg-white text-black' : 'bg-transparent text-white'}`}
           >
             Short Break
           </button>
           <button 
             onClick={() => switchMode('longBreak', modeDurations.longBreak)} 
-            className={`px-5 py-2 text-xl font-semibold rounded-3xl ${mode === 'longBreak' ? 'bg-white text-black' : 'bg-transparent text-white'}`}
+            className={`px-3 sm:px-5 py-2 text-sm sm:text-xl font-semibold rounded-3xl transition-all duration-200 ease-in ${mode === 'longBreak' ? 'bg-white text-black' : 'bg-transparent text-white'}`}
           >
             Long Break
           </button>
         </div>
 
-        <div className='flex justify-center items-center pt-12'>
-          <p className='text-black text-[5rem] font-semibold'>
+        <div className='flex justify-center items-center pt-20 sm:pt-12 transition-all duration-200 ease-in'>
+          <p className='text-black text-6xl sm:text-[5rem] font-semibold transition-all duration-200 ease-in'>
             {formatTime(timer)}
           </p>
         </div>
 
-        <div className='flex justify-center items-center pt-10 gap-7'>
+        <div className='flex justify-center items-center pt-20 sm:pt-10 gap-7 transition-all duration-200 ease-in'>
           <button 
             onClick={handleStartPause}
-            className='px-7 py-2 bg-[rgb(25,38,49)] text-white text-xl font-semibold rounded-3xl active:translate-y-1 '
+            className='px-7 py-2 bg-[rgb(25,38,49)] text-white text-base sm:text-xl font-semibold rounded-3xl active:translate-y-1 transition-all duration-200 ease-in'
           >
             {isActive ? "Pause" : "Start"}
           </button>
           <button onClick={handleRestart}>
             <MdRestartAlt 
-              size={'3.5rem'}
+              size={'3rem'}
               color='rgb(25,38,49)'
             />
           </button>
