3491c0c862b48c8932fb266903ffc402d94749da
Jonash <jonashtiglao@gmail.com>
2023-12-28 15:40:33 +0800

    fix mobile view

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { FaPlus } from "react-icons/fa6"
 import RenderTasks from '../components/RenderTasks'
 import Modal from '../components/Modal'
@@ -70,19 +70,31 @@ export default function Tasks() {
             <div className='max-w-2xl min-h-[400px] mx-auto bg-white px-4 py-4 rounded-lg flex justify-between flex-col'>
                 {isLoading ? (
                     <div className='w-full min-h-[400px] grid place-items-center'>
-                        <RotatingLines 
-                            strokeColor="rgb(21,128,61)"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            width="70"
-                            visible={true}
-                        />
+                        <div className='sm:block hidden'>
+                            <RotatingLines 
+                                strokeColor="rgb(21,128,61)"
+                                strokeWidth="5"
+                                animationDuration="0.75"
+                                width="70"
+                                visible={true}
+                            />
+                        </div>
+                        <div className='sm:hidden block'>
+                            <RotatingLines 
+                                strokeColor="rgb(21,128,61)"
+                                strokeWidth="5"
+                                animationDuration="0.75"
+                                width="50"
+                                visible={true}
+                            />
+                        </div>
+
                     </div>
                 ) : (
                     <>
-                        <div className='px-4 flex flex-col justify-between'>
-                            <h1 className='text-3xl font-bold pb-8 text-center'>{userInfo.name}&apos;s Tasks</h1>
-                            <ul className='w-[500px] flex gap-4 flex-col pb-8 mx-auto'>
+                        <div className='flex flex-col justify-between'>
+                            <h1 className='text-2xl sm:text-3xl font-bold pb-8 text-center transition-all duration-300 ease-in'>{userInfo.name}&apos;s Tasks</h1>
+                            <ul className='max-w-[500px] w-full flex gap-4 flex-col pb-8 mx-auto'>
                                 {todos.map((task, index) => (
                                     <RenderTasks 
                                         key={index} 
@@ -95,11 +107,11 @@ export default function Tasks() {
                             <Modal onClose={handleModalClose} isOpen={isOpen}/>
                         </div>
                         <button 
-                            className='flex items-center gap-2 text-lg px-3 py-3 bg-green-600 hover:bg-green-700 rounded-md text-white w-[125px] active:translate-y-2  transition-all duration-200 ease-in '
+                            className='flex items-center justify-center gap-2 sm:px-3 sm:py-3 px-2 py-2 bg-green-600 hover:bg-green-700 rounded-md text-white w-[125px] active:translate-y-2  transition-all duration-200 ease-in '
                             onClick={handleModalOpen}
                         >
                             <FaPlus size={'1.1rem'} color='white'/>
-                            Add Task
+                            <span className='text-base sm:text-lg'>Add Task</span>
                         </button>
                     </>
                 )}
