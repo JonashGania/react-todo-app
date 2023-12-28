468c83870c9a640fce74017034b0e779d749ca97
Jonash <jonashtiglao@gmail.com>
2023-12-28 15:38:38 +0800

    fix mobile view

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import { FaCheck } from "react-icons/fa";
 
 export default function Checkbox({ completed, toggleComplete }) {
   return (
     <div 
-        className={`w-5 h-5 rounded-[50%] flex justify-center items-center ${completed ? 'bg-green-600 border-none' : 'bg-transparent border-2 border-gray-400'}`}
+        className={`sm:w-5 sm:h-5 w-4 h-4 rounded-[50%] flex justify-center items-center ${completed ? 'bg-green-600 border-none' : 'bg-transparent border-2 border-gray-400'}`}
         onClick={toggleComplete}
     >
         <FaCheck 
